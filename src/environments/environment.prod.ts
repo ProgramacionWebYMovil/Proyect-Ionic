731182ae49343c4bda3e7bd21ae2d9b47e139527
KenaiGonzalezOrtega <113781990+KenaiGonzalezOrtega@users.noreply.github.com>
2023-05-10 10:50:36 +0100

    Arregladas dependencias de paquetes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'todo-ionic-10e77',
+    appId: '1:27198216413:web:cb7ed3bc1d38109f4f6334',
+    storageBucket: 'todo-ionic-10e77.appspot.com',
+    apiKey: 'AIzaSyDHOWB-Ki5Thx0PBm0ZajjxxYvov0FRimY',
+    authDomain: 'todo-ionic-10e77.firebaseapp.com',
+    messagingSenderId: '27198216413',
+    measurementId: 'G-NN75MKYQFD',
+  },
   production: true
 };
